ae2aa13c5b3dbc2e41e9e0e8e4628c2f94791dad
Graham Davison <g.m.davison@computer.org>
2014-05-23 16:09:48 -0700

    Add override function to Build action

--- actions.go
+++ actions.go
@@ -4,16 +4,16 @@ import (
 	"fmt"
 )
 
-func Build(name string) (interface{}, error) {
+func Build(name string, override Builder) (interface{}, error) {
 	factory := FactoryByName(name)
 	if factory == nil {
 		return nil, fmt.Errorf("factory_gorl: could not find Factory '%s'", name)
 	}
-	return factory.Run(), nil
+	return factory.Run(override), nil
 }
 
-func Create(name string) (interface{}, error) {
-	product, err := Build(name)
+func Create(name string, override Builder) (interface{}, error) {
+	product, err := Build(name, override)
 	if err != nil {
 		return nil, err
 	}
--- actions_test.go
+++ actions_test.go
@@ -14,8 +14,8 @@ type AutoIncrementIdTest struct {
 
 var _ = Describe("Build", func() {
 	var (
-		result interface{}
-		err    error
+		resultInterface interface{}
+		err             error
 	)
 	BeforeEach(func() { ResetConfiguration() })
 	Context("when there is a registered factory", func() {
@@ -23,29 +23,62 @@ var _ = Describe("Build", func() {
 			factory, _ := NewFactory("Test", Test{}, nil)
 			RegisterFactory(factory)
 		})
-		JustBeforeEach(func() {
-			result, err = Build("Test")
+		Context("when not overriding factory settings", func() {
+			JustBeforeEach(func() {
+				resultInterface, err = Build("Test", nil)
+			})
+			It("has a result value", func() {
+				Expect(resultInterface).ToNot(BeNil())
+			})
+			It("creates correct type", func() {
+				Expect(resultInterface).To(BeAssignableToTypeOf(&Test{}))
+			})
+			It("does not return an error", func() {
+				Expect(err).ToNot(HaveOccurred())
+			})
 		})
-		It("has a result value", func() {
-			Expect(result).ToNot(BeNil())
-		})
-		It("creates correct type", func() {
-			Expect(result).To(BeAssignableToTypeOf(&Test{}))
-		})
-		It("does not return an error", func() {
-			Expect(err).ToNot(HaveOccurred())
+		Context("when overriding factory settings", func() {
+			var (
+				result *Test
+			)
+			JustBeforeEach(func() {
+				resultInterface, err = Build("Test", func(i interface{}) {
+					t := i.(*Test)
+					t.foo = "thirteen"
+					t.bar = 13
+				})
+				result = resultInterface.(*Test)
+			})
+			It("sets the fields based on the override", func() {
+				Expect(result.foo).To(Equal("thirteen"))
+				Expect(result.bar).To(Equal(13))
+			})
+
 		})
 	})
 
 	Context("when there is not a registered factory", func() {
-		JustBeforeEach(func() {
-			result, err = Build("Nonesuch")
+		Context("when not overriding factory settings", func() {
+			JustBeforeEach(func() {
+				resultInterface, err = Build("Nonesuch", nil)
+			})
+			It("has no result value", func() {
+				Expect(resultInterface).To(BeNil())
+			})
+			It("returns an error", func() {
+				Expect(err).To(HaveOccurred())
+			})
 		})
-		It("has no result value", func() {
-			Expect(result).To(BeNil())
-		})
-		It("returns an error", func() {
-			Expect(err).To(HaveOccurred())
+		Context("when overriding factory settings", func() {
+			JustBeforeEach(func() {
+				resultInterface, err = Build("Nonesuch", func(i interface{}) {})
+			})
+			It("has no result value", func() {
+				Expect(resultInterface).To(BeNil())
+			})
+			It("returns an error", func() {
+				Expect(err).To(HaveOccurred())
+			})
 		})
 	})
 })
@@ -62,7 +95,7 @@ var _ = Describe("Create", func() {
 			RegisterFactory(factory)
 		})
 		JustBeforeEach(func() {
-			result, err = Create("AutoIncrementIdTest")
+			result, err = Create("AutoIncrementIdTest", nil)
 		})
 		It("has a result value", func() {
 			Expect(result).ToNot(BeNil())
@@ -84,7 +117,7 @@ var _ = Describe("Create", func() {
 
 	Context("when there is not a registered factory", func() {
 		JustBeforeEach(func() {
-			result, err = Create("Nonesuch")
+			result, err = Create("Nonesuch", nil)
 		})
 		It("has no result value", func() {
 			Expect(result).To(BeNil())
