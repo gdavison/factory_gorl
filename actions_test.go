75b684979b42a707021e1b2cd4f2f45a65db8089
Graham Davison <g.m.davison@computer.org>
2014-05-23 16:19:07 -0700

    Add override function to Create action

--- actions_test.go
+++ actions_test.go
@@ -94,36 +94,69 @@ var _ = Describe("Create", func() {
 			factory, _ := NewFactory("AutoIncrementIdTest", AutoIncrementIdTest{}, nil)
 			RegisterFactory(factory)
 		})
-		JustBeforeEach(func() {
-			result, err = Create("AutoIncrementIdTest", nil)
+		Context("when not overriding factory settings", func() {
+			JustBeforeEach(func() {
+				result, err = Create("AutoIncrementIdTest", nil)
+			})
+			It("has a result value", func() {
+				Expect(result).ToNot(BeNil())
+			})
+			It("creates correct type", func() {
+				Expect(result).To(BeAssignableToTypeOf(&AutoIncrementIdTest{}))
+			})
+			It("assigns an Id", func() {
+				Expect(result.(*AutoIncrementIdTest).Id).ToNot(Equal(0))
+			})
+			It("saves it to the database", func() {
+				retrieved, _ := DbMap.Get(AutoIncrementIdTest{}, result.(*AutoIncrementIdTest).Id)
+				Expect(retrieved.(*AutoIncrementIdTest).Name).To(Equal(result.(*AutoIncrementIdTest).Name))
+			})
+			It("does not return an error", func() {
+				Expect(err).ToNot(HaveOccurred())
+			})
 		})
-		It("has a result value", func() {
-			Expect(result).ToNot(BeNil())
-		})
-		It("creates correct type", func() {
-			Expect(result).To(BeAssignableToTypeOf(&AutoIncrementIdTest{}))
-		})
-		It("assigns an Id", func() {
-			Expect(result.(*AutoIncrementIdTest).Id).ToNot(Equal(0))
-		})
-		It("saves it to the database", func() {
-			retrieved, _ := DbMap.Get(AutoIncrementIdTest{}, result.(*AutoIncrementIdTest).Id)
-			Expect(retrieved.(*AutoIncrementIdTest).Name).To(Equal(result.(*AutoIncrementIdTest).Name))
-		})
-		It("does not return an error", func() {
-			Expect(err).ToNot(HaveOccurred())
+		Context("when overriding factory settings", func() {
+			JustBeforeEach(func() {
+				result, err = Create("AutoIncrementIdTest", func(i interface{}) {
+					t := i.(*AutoIncrementIdTest)
+					t.Name = "foobar"
+				})
+			})
+			It("sets the fields based on the override", func() {
+				Expect(result.(*AutoIncrementIdTest).Name).To(Equal("foobar"))
+			})
+
 		})
 	})
 
 	Context("when there is not a registered factory", func() {
+		var (
+			builder Builder
+		)
 		JustBeforeEach(func() {
 			result, err = Create("Nonesuch", nil)
 		})
-		It("has no result value", func() {
-			Expect(result).To(BeNil())
+		Context("when not overriding factory settings", func() {
+			BeforeEach(func() {
+				builder = nil
+			})
+			It("has no result value", func() {
+				Expect(result).To(BeNil())
+			})
+			It("returns an error", func() {
+				Expect(err).To(HaveOccurred())
+			})
 		})
-		It("returns an error", func() {
-			Expect(err).To(HaveOccurred())
+		Context("when not overriding factory settings", func() {
+			BeforeEach(func() {
+				builder = func(i interface{}) {}
+			})
+			It("has no result value", func() {
+				Expect(result).To(BeNil())
+			})
+			It("returns an error", func() {
+				Expect(err).To(HaveOccurred())
+			})
 		})
 	})
 })
